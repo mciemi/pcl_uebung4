0555ba23847eea1b2a39c28662c6e594f6d0c713
mciemi <monika.ciemiega@uzh.ch>
2017-04-25 12:17:59 +0200

    final changes

--- mciemi_pcl2_ue4_a1.py
+++ mciemi_pcl2_ue4_a1.py
@@ -8,62 +8,49 @@ from lxml import etree
 
 def getfreqwords(indir, outfile):
     """Creates file with the 20 most frequent sentences of the corpus"""
-   #indir: directory with all the xml files
-   #outfile: text file for the most frequent sentences
 
-    name = str(indr) + '\SAC_Jahrbuch*mul.xml'
-    filenames = glob.glob(name)
+    name = str(indir) + '\SAC_Jahrbuch*mul.xml'
 	
-    print("I have managed to open it")
-	
-    sentence_hashes = dict()
-    for name in filenames:
-        doc = etree.parse(name)
+    all_sentences = dict()
+    # The "[:2]" is the exit condition, if deleted it will go through all files.
+    for file in glob.glob(name)[:2]:
+        doc = etree.parse(file)
         root = doc.getroot()
         for child in root:
             for d in child:
     		#Going only through all "div" tags which include the sentences.
                 if d.tag == "div":
-                    for sent in d:
-				
-# Schritt 1: aus lemmas von satz neuen satz zusammenkleben		
-		                #Only if sentence has 6+ tokens, create lemmatised sentence.
+                    for sent in d:	
+                        #Only if sentence has 6+ tokens, create lemmatised sentence.
                         if len(sent)>5: 
                             sentence = ''
                             for w in sent:
-				                #Create lemmatised sentence to get its hash value.
+				# Create lemmatised sentence to get its hash value.
                                 try:
                                     sentence += w.get("lemma")
                                     sentence += " "
-				                #Exception for when it's not a "w" tag and the w.get is None.
+				#Exception for when it's not a "w" tag and the w.get is None.
                                 except TypeError:
-                                    pass
-						
-# Schritt 3: hash zu dict, prüfen ob schon mal drin
-		    			#Check if sentence has been used before and add to/increase dict.
-                        if sentence not in sentence_hashes:
-                            sentence_hashes[sentence] = 1					
+                                    pass			
+		    	#Check if sentence has been used before and add to/increase dict.
+                        if sentence not in all_sentences:
+                            all_sentences[sentence] = 1					
                         else:
-                            sentence_hashes[sentence] += 1
-						#Delete the s_hash variable for each sentence to free memory.
-                        del sentence
-
-    print("I have gone through all sentences")						
-	
+                            all_sentences[sentence] += 1
+                        #Delete the sentence variable for each sentence to free memory.
+                        del sentence					
     #Create a new list with the 20 most frequent hashes.
-    top_hashes = sorted(sentence_hashes, key=sentence_hashes.get, reverse=True)[0:20]
+    top_hashes = sorted(all_sentences, key=all_sentences.get, reverse=True)[0:20]
     #Delete the dictionary of all hashes to free memory space.
     del sentence_hashes
 	
-# Schritt 5: häufigste 20 finden und ausschreiben
     #Search for the sentences to the top 20 hashes, write them into outfile.
     for s in top_hashes:
         outfile.write(s + "\n")
-	
-   
+	 
    
 def main():
-    i = "C:\Users\User\Documents\UZH\PCL\PCL ubungen\ubung 4\SAC"
+    i = r"C:\Users\User\Documents\UZH\PCL\PCL ubungen\ubung 4\SAC"
     o = open("top_20_sentences.txt", 'w')
     getfreqwords(i, o)
 
